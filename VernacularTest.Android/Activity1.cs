eafa91bbd7e1314916885a88c95353965aca64a7
Jonathan Peppers <jonathan.peppers@gmail.com>
2012-11-20 12:01:46 -0600

    Added Windows and Android projects

---
+++ VernacularTest.Android/Activity1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+using Vernacular;
+
+namespace VernacularTest.Android {
+    [Activity (Label = "VernacularTest.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity {
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button> (Resource.Id.MyButton);
+
+            button.Click += delegate {
+                var manager = new ResourceManager ("VernacularTest.Windows.Strings", Assembly.GetExecutingAssembly ());
+
+                Catalog.Implementation = new AndroidCatalog (Resources, typeof (Resource.String));
+                Catalog.ErrorHandler = (exc, text) => {
+                    Console.WriteLine ("Error: " + exc);
+                };
+
+                string english = Catalog.GetString ("Test");
+                Console.WriteLine ("English: " + english);
+
+                //Change to french
+                var locale = new Locale ("fr");
+                Locale.Default = locale;
+                Configuration config = new Configuration ();
+                config.Locale = locale;
+                ApplicationContext.Resources.UpdateConfiguration (config, null);
+
+                string french = Catalog.GetString ("Test");
+                Console.WriteLine ("French: " + french);
+            };
+        }
+    }
+}
+
---
+++ VernacularTest.Windows/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Vernacular;
+
+namespace VernacularTest.Windows {
+    class Program {
+        static void Main (string [] args)
+        {
+            var manager = new ResourceManager("VernacularTest.Windows.Strings", Assembly.GetExecutingAssembly());
+
+            Catalog.Implementation = new ResourceCatalog {
+                GetResourceById = id => manager.GetString (id),
+            };
+            Catalog.ErrorHandler = (exc, text) => {
+                Console.WriteLine ("Error: " + exc);
+            };
+
+            string english = Catalog.GetString ("Test");
+            Console.WriteLine ("English: " + english);
+
+            //Change to French
+            Thread.CurrentThread.CurrentCulture = 
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo ("fr-FR");
+
+            string french = Catalog.GetString ("Test");
+            Console.WriteLine ("French: " + french);
+
+            Console.WriteLine ("Press enter to quit...");
+            Console.ReadLine ();
+        }
+    }
+}
